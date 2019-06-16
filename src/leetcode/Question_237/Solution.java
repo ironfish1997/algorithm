5c5736333625caaefe02d4eb1a35e6aae788838a
ironfish1997 <1481980097@qq.com>
2019-06-16 16:04:23 +0800

    update

---
+++ src/leetcode/Question_237/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.Question_237;
+
+import leetcode.common.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
