205e92d393b21e28ddc00c5609a2bc1e65ad8ec1
ironfish1997 <1481980097@qq.com>
2019-07-12 15:09:20 +0800

    update

--- src/leetcode/Question_279/Solution.java
+++ src/leetcode/Question_279/Solution.java
@@ -1,10 +1,22 @@
-//package leetcode.Question_279;
-//
-//class Solution {
-//    public int numSquares(int n) {
-//        if (n <= 0) {
-//            return 0;
-//        }
-//
-//    }
-//}
+package leetcode.Question_279;
+
+class Solution {
+    public int numSquares(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        return findMinCompose(n);
+    }
+
+    //找到最小组合
+    private int findMinCompose(int n) {
+        if (n == 0) {
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String s = "123";
+        System.out.println(s.substring(0, 1));
+    }
+}
