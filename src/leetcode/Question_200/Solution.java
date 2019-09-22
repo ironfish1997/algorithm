897db16e58a061654eb4c64daee50cac2e9f4476
ironfish1997 <1481980097@qq.com>
2019-09-22 23:43:10 +0800

    update question 200

--- src/leetcode/Question_200/Solution.java
+++ src/leetcode/Question_200/Solution.java
@@ -5,7 +5,38 @@ package leetcode.Question_200;
  * @Date 2019-09-22
  **/
 class Solution {
-    public int numIslands(char[][] grid) {
+    private int[][] steps = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private int m, n;
+    private boolean[][] visited;
 
+    public int numIslands(char[][] grid) {
+        m = grid.length;
+        if (m == 0) {
+            return 0;
+        }
+        n = grid[0].length;
+        visited = new boolean[m][n];
+        int res = 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    res++;
+                    dfs(grid, i, j);
+                }
+        return res;
+    }
+
+    private void dfs(char[][] grid, int x, int y) {
+        visited[x][y] = true;
+        for (int i = 0; i < 4; i++) {
+            int newX = x + steps[i][0];
+            int newY = y + steps[i][1];
+            if (inArea(newX, newY) && !visited[newX][newY] && grid[newX][newY] == '1') dfs(grid, newX, newY);
+
+        }
+    }
+
+    private boolean inArea(int x, int y) {
+        return x >= 0 && x < m && y >= 0 && y < n;
     }
 }
