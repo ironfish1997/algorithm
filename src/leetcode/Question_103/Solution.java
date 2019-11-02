2b4fcd55bb857e3efe1627b2087ceee9b52ce804
liyong.liu <liyong.liu@yitu-inc.com>
2019-11-02 15:05:57 +0800

    Update Solution for question 103

--- src/leetcode/Question_103/Solution.java
+++ src/leetcode/Question_103/Solution.java
@@ -19,28 +19,28 @@ import java.util.Queue;
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        if(root == null)
+        if (root == null)
             return result;
         zigzagRecursive(0, root, result);
         return result;
     }
 
-    private void zigzagRecursive(int level, TreeNode root, List<List<Integer>> result){
-        if(root==null)
+    private void zigzagRecursive(int level, TreeNode root, List<List<Integer>> result) {
+        if (root == null)
             return;
         LinkedList<Integer> row;
-        if(result.size()<=level){
+        if (result.size() <= level) {
             row = new LinkedList<>();
-            result.add(level,row);
-        }else
-            row = (LinkedList)result.get(level);
+            result.add(level, row);
+        } else
+            row = (LinkedList<Integer>) result.get(level);
 
-        if(level%2==0){
+        if (level % 2 == 0) {
             row.add(root.val);
-        }else{
+        } else {
             row.addFirst(root.val);
         }
-        zigzagRecursive(level+1, root.left,result);
-        zigzagRecursive(level+1, root.right,result);
+        zigzagRecursive(level + 1, root.left, result);
+        zigzagRecursive(level + 1, root.right, result);
     }
 }
