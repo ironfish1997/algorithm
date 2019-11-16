e762a78f01a99d54acc8eb25721a61265ef1db58
liyong.liu <liyong.liu@yitu-inc.com>
2019-11-16 15:58:10 +0800

    create solution for Question 50

--- src/leetcode/Question_34/Solution.java
+++ src/leetcode/Question_34/Solution.java
@@ -6,28 +6,28 @@ package leetcode.Question_34;
  */
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if(nums==null||nums.length==0||target>nums[nums.length-1]||target<nums[0]){
-            return new int[]{-1,-1};
+        if (nums == null || nums.length == 0 || target > nums[nums.length - 1] || target < nums[0]) {
+            return new int[]{-1, -1};
         }
-        int left = 0, right = nums.length-1,mid = left+ (right-left)/2;
-        while(left<=right){
-            if(nums[mid]==target){
+        int left = 0, right = nums.length - 1, mid = left + (right - left) / 2;
+        while (left <= right) {
+            if (nums[mid] == target) {
                 int i = mid;
                 int j = mid;
-                while(i-1>=left&&nums[i-1]==target){
+                while (i - 1 >= left && nums[i - 1] == target) {
                     i--;
                 }
-                while(j+1<=right&&nums[j+1]==target){
+                while (j + 1 <= right && nums[j + 1] == target) {
                     j++;
                 }
-                return new int[]{i,j};
-            }else if(nums[mid]<=target){
-                left = mid+1;
-            }else{
-                right = mid -1;
+                return new int[]{i, j};
+            } else if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
-            mid = left+ (right-left)/2;
+            mid = left + (right - left) / 2;
         }
-        return new int[]{-1,-1};
+        return new int[]{-1, -1};
     }
 }
